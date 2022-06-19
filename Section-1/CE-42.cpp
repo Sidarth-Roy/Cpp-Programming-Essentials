c36fa45cb419e79e2f288fc7b2970d057f9759ed
Winter <73904891+Sidarth-Roy@users.noreply.github.com>
2022-06-19 17:06:08 +0530

    Create CE-42.cpp

---
+++ Section-1/CE-42.cpp
@@ -0,0 +1,12 @@
+//Coding Exercise 42: Factorial using recursion
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n) {
+    
+    if(n==1||n==0)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
