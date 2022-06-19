5080fa00799c427542472f6745b18e1992b78dc2
Winter <73904891+Sidarth-Roy@users.noreply.github.com>
2022-06-19 17:07:53 +0530

    Create CE-44.cpp

---
+++ Section-1/CE-44.cpp
@@ -0,0 +1,13 @@
+//Coding Exercise 44: Fibonacci series 2
+#include<bits/stdc++.h>
+using namespace std;
+
+int fibonacciSeries(int n) {
+    if(n==0|| n==1||n==2)
+    {
+        return 1;
+    }
+
+    return fibonacciSeries(n-1)+fibonacciSeries(n-2)+fibonacciSeries(n-3);
+
+}
