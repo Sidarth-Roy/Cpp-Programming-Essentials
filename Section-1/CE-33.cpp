0d6e294436459705cfccee7d4dd7f9efea857e0b
Winter <73904891+Sidarth-Roy@users.noreply.github.com>
2022-06-19 17:01:06 +0530

    Create CE-33.cpp

---
+++ Section-1/CE-33.cpp
@@ -0,0 +1,22 @@
+//Coding Exercise 33: All Alphabets present or not ?
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int allAlphabetsPresentOrNot(string s) {
+    
+    int check[26] = {0};
+    
+    for (int i = 0; i < int(s.size()); i++) {
+        check[int(s[i]-'a')]++;
+    }
+    
+    for (int i = 0; i < 26; i++) {
+        if (check[i] == 0) {
+            return 0;
+        }
+    }
+    
+    return 1;
+    
+}
